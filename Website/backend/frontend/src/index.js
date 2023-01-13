40e3ad57982249c54cc842dc7b65adbde6758f0e
Manav108 <abhmanav0103@gmail.com>
2023-01-14 05:08:40 +0530

    Upload all files
    added public folder and other too

--- Website/backend/frontend/src/index.js
+++ Website/backend/frontend/src/index.js
@@ -1,21 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import './index.css';
-//  import App from './App';
-// import Login from './Login.js'
-// import './Login.css';
-// import './SignUp.css';
-// import SignUp from './SignUp.js'
-import './Aform.css';
-import Aform from './Aform.js'
+import Home from './home.js';
+import Login from './Login.js';
+import SignUp from './SignUp.js';
+import Application from './Aform.js';
+import Navigation from './Navigation.js';
 import reportWebVitals from './reportWebVitals';
+
+const WebPages = () => {
+  document.title = "Railway Concession";
+  return (
+    <section className="listofwebpages">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="ApplicationForm" element={<Application />} />
+          <Route path="SignUp" element={<SignUp />} />
+          <Route path="Login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </section>
+  )
+}
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <Login /> */}
-    {/* <SignUp/> */}
-    <Aform/>
+    {/* <SignUp /> */}
+    {/* <Application /> */}
+    {/* <Home /> */}
+    <WebPages />
   </React.StrictMode>
 );
 
