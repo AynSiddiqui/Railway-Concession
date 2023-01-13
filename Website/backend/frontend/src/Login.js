722046afef975a55d0dce117321d574481632e47
Manav108 <abhmanav0103@gmail.com>
2023-01-14 05:26:16 +0530

    Update Login Page
    Added Home Page link in Login Page

--- Website/backend/frontend/src/Login.js
+++ Website/backend/frontend/src/Login.js
@@ -6,7 +6,7 @@ function Login() {
     return (
         <div>
             <div className="h-screen bgimage flex justify-center items-center ">
-                <div className="bg-white w-[500px] h-[520px] rounded-3xl flex flex-col space-y-10 justifiy-center items-center">
+                <div className="bg-white w-[500px] h-[540px] rounded-3xl flex flex-col space-y-10 justifiy-center items-center">
                     <h1 className="text-4xl text-black font-bold mt-8">Log In</h1>
                     <form>
                         <div className="my-1">
@@ -21,9 +21,10 @@ function Login() {
                         </div>
                         <Link to='/'><button type="submit" className="inline-block px-6 py-2.5 bg-white text-pink-violent font-medium text-lg leading-tight uppercase rounded-full shadow-md hover:bg-red-600 hover:text-white hover:shadow-lg focus:bg-pink-violent focus:text-white focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-violent active:text-white active:shadow-lg transition duration-150 ease-in-out">Submit</button></Link>
                     </form>
-                    <div className="my-2 text-white">
+                    <div className="my-2 text-white flex flex-col justify-center items-center space-y-1">
                         <p className="text-xl text-black">Don't have account?</p>
                         <Link to='/SignUp' className="text-2xl text-black text-center underline cursor-pointer hover:text-red-600">Create Account</Link>
+                        <div><Link to='/' className="text-xl text-black text-center cursor-pointer hover:text-red-600">Home Page</Link></div>
                     </div>
                 </div>
             </div>
