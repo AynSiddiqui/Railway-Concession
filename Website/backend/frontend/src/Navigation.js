6de806ea781cdb4154bf62cb72b42c2872650352
Manav108 <abhmanav0103@gmail.com>
2023-01-14 08:15:57 +0530

    Update Login Page and NavBar
    Image change in Login Page and NavBar

--- Website/backend/frontend/src/Navigation.js
+++ Website/backend/frontend/src/Navigation.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Login from './Login.js';
-import SignUp from './SignUp.js';
-import Application from './Aform.js';
+import NavigationImage from './NavigationImage.png';
 import "./Navigation.css";
 
 function Navigation() {
@@ -10,7 +8,7 @@ function Navigation() {
         <>
             <div className="bg-gradient-to-bl from-purple-200 via-purple-400 to-purple-800 ">
                 <nav className="flex flex-row h-16 ">
-                    <img src="https://o.remove.bg/downloads/7dc3fdfa-f3dc-420d-bb38-62d7f0a21051/image-removebg-preview.png" className="h-16 pl-4 pt-3 animate-bounce" alt=""></img>
+                    <img src={NavigationImage} className="h-16 pl-4 pt-3 animate-bounce" alt=""></img>
                     <div className="flex list-none my-auto ml-[800px] space-x-32">
                         <Link to='/ApplicationForm' className='text-black p-2 font-semibold hover:bg-purple-500 hover:text-white rounded-lg hover:cursor-pointer'>Application Form</Link>
                         <Link to='/SignUp' className='text-black p-2 font-semibold hover:bg-purple-500 hover:text-white rounded-lg'>Sign Up</Link>
