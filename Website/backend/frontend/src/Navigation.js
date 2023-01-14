29c90e89f6d7ec351ba54ac2049661e0e54c7dc9
Manav108 <abhmanav0103@gmail.com>
2023-01-14 09:49:02 +0530

    Added Documentation
    Download Pdf and Navigation Change

--- Website/backend/frontend/src/Navigation.js
+++ Website/backend/frontend/src/Navigation.js
@@ -3,13 +3,26 @@ import { Link } from "react-router-dom";
 import NavigationImage from './NavigationImage.png';
 import "./Navigation.css";
 
-function Navigation() {
+const Navigation = () => {
+  
+    const onButtonClick = () => {
+        fetch('Documentation.pdf').then(response => {
+            response.blob().then(blob => {
+                const fileURL = window.URL.createObjectURL(blob);
+                let alink = document.createElement('a');
+                alink.href = fileURL;
+                alink.download = 'Documentation.pdf';
+                alink.click();
+            })
+        })
+    }
     return (
         <>
             <div className="bg-gradient-to-bl from-purple-200 via-purple-400 to-purple-800 ">
                 <nav className="flex flex-row h-16 ">
                     <img src={NavigationImage} className="h-16 pl-4 pt-3 animate-bounce" alt=""></img>
-                    <div className="flex list-none my-auto ml-[800px] space-x-32">
+                    <div className="flex list-none my-auto ml-[800px] space-x-16">
+                    <button className='text-black p-2 font-semibold hover:bg-purple-500 hover:text-white rounded-lg' onClick={onButtonClick} >Documentation</button>
                         <Link to='/ApplicationForm' className='text-black p-2 font-semibold hover:bg-purple-500 hover:text-white rounded-lg hover:cursor-pointer'>Application Form</Link>
                         <Link to='/SignUp' className='text-black p-2 font-semibold hover:bg-purple-500 hover:text-white rounded-lg'>Sign Up</Link>
                         <Link to='/Login' className='text-black p-2 font-semibold hover:bg-purple-500 hover:text-white rounded-lg'>Sign In</Link>
