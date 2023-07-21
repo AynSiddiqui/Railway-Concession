9b85cf3bc6e1ba0a0965afa7d50cf6a055e3780e
roze-ferns <rozeeta1107@gmail.com>
2023-07-21 16:36:54 +0530

    Added concession slip pdf generation function

--- Website/backend/frontend/src/GenerateSlip.js
+++ Website/backend/frontend/src/GenerateSlip.js
@@ -1,47 +1,34 @@
-// import React from "react";
-import Navigation from "./Navigation.js";
-import React, { useState, useEffect } from "react";
-import Footer from "./Footer.js";
+import React, { useRef, useEffect } from "react";
+import Slip from "./Slip";
+import html2pdf from "html2pdf.js";
 
 function GenerateSlip() {
-  const handleGenerateSlip = () => {
-    // Logic for generating the slip
+  const pdfGeneratorRef = useRef(null);
+
+  const handleDownloadPDF = () => {
+    if (pdfGeneratorRef.current) {
+      pdfGeneratorRef.current.generatePDF();
+    }
   };
 
-  const [isPageLoaded, setPageLoaded] = useState(false);
-
   useEffect(() => {
-    // Set a delay of 100ms to show the page content after the fade-in effect
-    setTimeout(() => {
-      setPageLoaded(true);
-    }, 100);
+    // When the page loads, check if there is data in localStorage
+    const pdfData = localStorage.getItem("pdfData");
+    if (pdfData) {
+      const formData = JSON.parse(pdfData);
+      // Render the Slip component with the data
+      pdfGeneratorRef.current.setFormUsers(formData);
+    }
   }, []);
 
   return (
     <div>
-      <Navigation />
-      <div
-        className={`h-screen flex justify-center items-center bg-cover bg-center bg-no-repeat bg-picSignUp `}
-      >
-        {/* <div
-          className={`bg-white w-[500px] h-[520px] rounded-3xl flex flex-col space-y-10 justifiy-center items-center transition-opacity duration-1000 ${
-            isPageLoaded ? "opacity-100" : "opacity-0"
-          }`}
-        > */}
-        {/* <h1 className="text-4xl text-black font-bold mt-8">Generate slip</h1> */}
-
-        <button
-          type="submit"
-          className="inline-block px-6 py-2.5 bg-white text-pink-violent font-medium text-lg leading-tight uppercase rounded-full shadow-md hover:dark:bg-gray-900 hover:text-white hover:shadow-lg focus:bg-pink-violent focus:text-white focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-violent active:text-white active:shadow-lg transition duration-150 ease-in-out"
-          onClick={handleGenerateSlip}
-        >
-          Generate slip
-        </button>
+      {/* ... your existing JSX ... */}
+      <button onClick={handleDownloadPDF}>Download PDF</button>
+      <div style={{ display: "none" }}>
+        {/* Render the Slip component but keep it hidden */}
+        <Slip ref={pdfGeneratorRef} />
       </div>
-      {/* </div> */}
-      {/* <h1>Generate Slip</h1>
-      <button onClick={handleGenerateSlip}>Generate Slip</button> */}
-      <Footer />
     </div>
   );
 }
--- Website/backend/frontend/src/Slip.js
+++ Website/backend/frontend/src/Slip.js
@@ -1,35 +1,12 @@
 // import "./Slip.css";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import html2pdf from "html2pdf.js";
 import "./Slip.css";
+import Navigation from "./Navigation.js";
+import Footer from "./Footer.js";
 
-// const fetchFormUsers = async () => {
-//   try {
-//     const response = await axios.get(
-//       // `http://localhost:5000/api/formAuth/formusers?regId=${regId}`
-//       "http://localhost:5000/api/formAuth/formusers"
-//     );
-//     return response.data;
-//   } catch (error) {
-//     console.error(error);
-//     return null;
-//   }
-// };
-
-function Slip() {
-  // const [formUsers, setFormUsers] = useState([]);
-
-  // useEffect(() => {
-  //   const fetchUsers = async () => {
-  //     const users = await fetchFormUsers();
-  //     if (users) {
-  //       setFormUsers(users);
-  //     }
-  //   };
-
-  //   fetchUsers();
-  // }, []);
-  // const [userDetails, setUserDetails] = useState("");
+function Slip({ isButtonClick }) {
   const [FormUser, setFormUsers] = useState("");
   console.log(localStorage.getItem("userRegId"));
   const loggedInUserRegId = localStorage.getItem("userRegId");
@@ -49,140 +26,182 @@ function Slip() {
     }
   };
 
+  const downloadAsPDF = () => {
+    const contentDiv = document.getElementById("pdf-content");
+
+    const opt = {
+      margin: 10,
+      filename: "slip.pdf",
+      image: { type: "jpeg", quality: 0.98 },
+      html2canvas: { scale: 2 },
+      jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
+    };
+
+    html2pdf().from(contentDiv).set(opt).save();
+  };
+
   return (
-    <div className="h-screen flex justify-center items-center ">
-      <div className="center-rectangle">
-        <div className="rectangle">
-          <div id="centerthis ">
-            <div className="container">
-              {/* // */}
-              <div className="d-flex justify-content-center">
-                <div className="center-table">
-                  <table border="0" cellSpacing="0" cellPadding="0">
-                    <tr>
-                      <td className="image-cell" valign="top" bgcolor="#ffe77b">
-                        <img
-                          src="https://img.collegepravesh.com/2016/01/VJTI-Mumbai-Logo.png"
-                          style={{
-                            height: "100%",
-                            width: "100%",
-                            transform: "rotate(0deg)",
-                          }}
-                          alt=""
-                        />
-                      </td>
-                      <td className="text-cell" bgcolor="#ffe77b">
-                        <h2>Railway Concession Slip</h2>
+    // <div className="h-screen flex justify-center items-center ">
+    <>
+      <Navigation />
+      <div className="hey">
+        <div
+          className="button flex justify-center items-center "
+          style={{ marginTop: "10vh" }}
+        >
+          <h1 style={{ fontSize: "24px", fontWeight: "bold", color: "#333" }}>
+            SLIP PREVIEW
+          </h1>
+        </div>
+
+        {/* <div className="bg-cover bg-center bg-no-repeat bg-picSignUp"> */}
+        <div className=" flex justify-center items-center  ">
+          <div id="pdf-content" className="center-rectangle">
+            {/* <div className="center-rectangle"> */}
+            <div className="rectangle">
+              <div id="centerthis ">
+                <div className="container">
+                  {/* // */}
+                  <div className="d-flex justify-content-center">
+                    <div className="center-table">
+                      <table border="0" cellSpacing="0" cellPadding="0">
+                        <tr>
+                          <td
+                            className="image-cell"
+                            valign="top"
+                            bgcolor="#ffe77b"
+                          >
+                            <img
+                              src="https://img.collegepravesh.com/2016/01/VJTI-Mumbai-Logo.png"
+                              style={{
+                                height: "100%",
+                                width: "100%",
+                                transform: "rotate(0deg)",
+                              }}
+                              alt=""
+                            />
+                          </td>
+                          <td className="text-cell" bgcolor="#ffe77b">
+                            <h2>Railway Concession Slip</h2>
+                            <p>
+                              Veermata Jijabai Technological Institute, Matunga,
+                              Mumbai
+                              <br />
+                              <a href="http://vjti.ac.in">
+                                http://vjti.ac.in
+                              </a>{" "}
+                              | vjti@institute.com +915 7 789-1234
+                            </p>
+                          </td>
+                        </tr>
+                      </table>
+                    </div>
+                  </div>
+                  <div className="center-table">
+                    <div className="mt-4 row justify-content-left">
+                      <div
+                        key={FormUser._id}
+                        className="mt-4 row justify-content-left"
+                      >
+                        <div className="border-right border-dark col-2">
+                          <p>
+                            <b>Name :</b> {FormUser.firstname}{" "}
+                            {FormUser.middlename} {FormUser.surname}
+                          </p>
+                        </div>
+                        <div className="col-2">
+                          <p>
+                            <b>Validity :</b> <span>{FormUser.startdate}</span>{" "}
+                            <b>-</b> <span>{FormUser.enddate}</span>
+                          </p>
+                        </div>
+                        <div className="col-2">
+                          <p>
+                            {" "}
+                            <b> Duration :</b> {FormUser.duration}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="center-table">
+                    <div className="mt-2 row justify-content-left">
+                      <div className="col-2">
                         <p>
-                          Veermata Jijabai Technological Institute, Matunga,
-                          Mumbai
-                          <br />
-                          <a href="http://vjti.ac.in">http://vjti.ac.in</a> |
-                          vjti@institute.com +915 7 789-1234
+                          {" "}
+                          <b> Class type : </b>
+                          {FormUser.class1}{" "}
                         </p>
-                      </td>
-                    </tr>
-                  </table>
-                </div>
-              </div>
-              <div className="center-table">
-                <div className="mt-4 row justify-content-left">
-                  <div
-                    key={FormUser._id}
-                    className="mt-4 row justify-content-left"
-                  >
-                    <div className="border-right border-dark col-2">
-                      <p>
-                        <b>Name :</b> {FormUser.firstname} {FormUser.middlename}{" "}
-                        {FormUser.surname}
-                      </p>
-                    </div>
-                    <div className="col-2">
-                      <p>
-                        <b>Validity :</b> <span>{FormUser.startdate}</span>{" "}
-                        <b>-</b> <span>{FormUser.enddate}</span>
-                      </p>
-                    </div>
-                    <div className="col-2">
-                      <p>
-                        {" "}
-                        <b> Duration :</b> {FormUser.duration}
-                      </p>
+                      </div>
+                      <div className="col-2">
+                        <p>
+                          <b>Station :</b> <span>{FormUser.stationfrom}</span>{" "}
+                          <b>-</b> <span>{FormUser.stationto}</span>
+                        </p>
+                      </div>
+                      <div className="col-2">
+                        <p>
+                          {" "}
+                          <b>Category : </b>
+                          {FormUser.category}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </div>
 
-              <div className="center-table">
-                <div className="mt-2 row justify-content-left">
-                  <div className="col-2">
-                    <p>
-                      {" "}
-                      <b> Class type : </b>
-                      {FormUser.class1}{" "}
-                    </p>
-                  </div>
-                  {/* <div className="col-2">
-                    <p>Type: </p>
-                  </div> */}
-                  <div className="col-2">
-                    <p>
-                      <b>Station :</b> <span>{FormUser.stationfrom}</span>{" "}
-                      <b>-</b> <span>{FormUser.stationto}</span>
-                    </p>
-                  </div>
-                  <div className="col-2">
-                    <p>
-                      {" "}
-                      <b>Category : </b>
-                      {FormUser.category}
-                    </p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="center-table">
-                <div className="mt-2 row justify-content-left">
-                  <div className="col-3">
-                    <p>
-                      <b>Ticket ID : </b> {FormUser.ticketNo}
-                    </p>
-                  </div>
-                  <div className="col-2">
-                    {/* <img
-              src="./Approved.png"
-              style={{ height: "100%", width: "150%" }}
-              alt="Approved"
-            /> */}
-                    <div className="col-2 flex">
-                      <img
-                        // src="https://www.onlygfx.com/wp-content/uploads/2016/09/green-approved-stamp-3.png"
-                        src="https://www.pngplay.com/wp-content/uploads/6/Green-Approved-Vector-Transparent-PNG.png"
-                        style={{
-                          height: "30%",
-                          width: "25%",
-                          marginTop: "-100px",
-                          marginLeft: "400px",
-                          transform: "rotate(0deg)",
-                        }}
-                        // style={{
-                        //   height: "15%",
-                        //   width: "20%",
-                        //   marginTop: "-15px",
-                        //   marginLeft: "10px",
-                        //   transform: "rotate(0deg)",
-                        // }}
-                        alt=""
-                      />
+                  <div className="center-table">
+                    <div className="mt-2 row justify-content-left">
+                      <div className="col-3">
+                        <p>
+                          <b>Ticket ID : </b> {FormUser.ticketNo}
+                        </p>
+                      </div>
+                      <div className="col-2">
+                        <div className="col-2 flex">
+                          <img
+                            // src="https://www.onlygfx.com/wp-content/uploads/2016/09/green-approved-stamp-3.png"
+                            src="https://www.pngplay.com/wp-content/uploads/6/Green-Approved-Vector-Transparent-PNG.png"
+                            style={{
+                              height: "30%",
+                              width: "25%",
+                              marginTop: "-100px",
+                              marginLeft: "400px",
+                              transform: "rotate(0deg)",
+                            }}
+                            // style={{
+                            //   height: "15%",
+                            //   width: "20%",
+                            //   marginTop: "-15px",
+                            //   marginLeft: "10px",
+                            //   transform: "rotate(0deg)",
+                            // }}
+                            alt=""
+                          />
+                        </div>
+                      </div>
                     </div>
+                    {/* </div> */}
                   </div>
                 </div>
               </div>
             </div>
           </div>
+          {/* /// */}
         </div>
+        <div className="button flex justify-center items-center ">
+          <button
+            type="submit"
+            className="inline-block px-6 py-2.5 bg-dark:bg-gray-900 text-pink-violent font-medium text-lg leading-tight uppercase rounded-full shadow-md hover:dark:bg-gray-900 hover:text-white hover:shadow-lg focus:bg-pink-violent focus:text-white focus:shadow-lg focus:outline-none focus:ring-0 active:bg-pink-violent active:text-white active:shadow-lg transition duration-150 ease-in-out"
+            onClick={downloadAsPDF}
+          >
+            Download Concession slip
+          </button>
+        </div>
+        {/* </div> */}
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 
